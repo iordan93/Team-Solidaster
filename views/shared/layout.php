68e5fe3e527e52cbec99e62cd39094e6acf4d95a
Yordan Darakchiev <iordan93@gmail.com>
2014-08-25 21:00:55 +0300

    Message sending, styles, and other fixes

--- views/shared/layout.php
+++ views/shared/layout.php
@@ -1,9 +1,9 @@
 <?php require_once realpath("views/elements/header.php"); ?>
-    <div>
+    <div id="wrapper">
         <?php require_once realpath("views/elements/message.php"); ?>
-        <main class='col-lg-8 col-md-8 col-sm-8 col-xs-8'>
+        <main class='col-lg-9 col-md-9 col-sm-9 col-xs-9'>
             <?php require_once $templateFileName; ?>
         </main>
         <?php require_once realpath("views/elements/sidebar.php"); ?>
+        <?php require_once realpath("views/elements/footer.php"); ?>
     </div>
-<?php require_once realpath("views/elements/footer.php"); ?>
