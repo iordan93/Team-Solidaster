32b5d561f299a6cd39255d5beffd198e86db43f4
Krasimir <k_garbuchev@yahoo.com>
2014-08-26 18:47:31 +0300

    Categories View Update

--- views/categories/view.php
+++ views/categories/view.php
@@ -16,14 +16,14 @@
             <li>
                 <div class="panel panel-primary">
                     <div class="panel-heading">
-                        <a href="../questions/details/<?= $question['id']?>">
-                        <h3><?= $question['title'] ?></h3>
+                        <a href="<?= ABS_ROOT_URL ?>questions/details/<?= $question['id'] ?>">
+                            <h3><?= $question['title'] ?></h3>
                         </a>
                     </div>
                     <div class="panel-body">
                         <a href="#" class="btn btn-info">Votes</a>
                         <a href="#" class="btn btn-success">Answers</a>
-<!--                        <p class="text-info">--><?//= $question['text'] ?><!--</p>-->
+                        <!--                        <p class="text-info">--><? //= $question['text'] ?><!--</p>-->
 
 
                     </div>
@@ -36,8 +36,3 @@
     </ul>
 </div>
 <?php
-
-
-//var_dump($category);
-//var_dump($questions);
-//var_dump($query);
