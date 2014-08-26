e4ccf6c63f24284b8e7dbd33d77d3193ea2ae314
DinkoAndreev <dinkoandreev@gmail.com>
2014-08-26 14:39:28 +0300

    add category style

--- views/categories/add.php
+++ views/categories/add.php
@@ -1,5 +1,24 @@
-<form method='post'>
-    Category name: <input type='text' name='catName' required/>
-    Category description: <textarea name='catDescription' required /></textarea>
-    <input type='submit' value='Add category' />
-</form>
+<div class="well bs-component">
+    <form method="post" class="form-horizontal">
+        <fieldset>
+            <legend>Add category</legend>
+            <div class="form-group">
+                <label for="catName" class="col-lg-2 control-label">Category name</label>
+                <div class="col-lg-10">
+                    <input type="text" class="form-control" ID="catName" name='catName' placeholder="Category name" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="catDescription" class="col-lg-2 control-label">Category description</label>
+                <div class="col-lg-10">
+                    <input type="text" class="form-control" id="catDescription" name='catDescription' placeholder="Category description" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-lg-10 col-lg-offset-2">
+                    <input type="submit" class="btn btn-primary" value="Add category"/>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+</div>
