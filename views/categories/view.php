d347b2af9551adbed53fbf07d78542f1e820b1da
Krasimir <k_garbuchev@yahoo.com>
2014-08-27 00:34:02 +0300

    Categories View Last Update

--- views/categories/view.php
+++ views/categories/view.php
@@ -1,36 +1,81 @@
-<div class="col-sm-12 clearfix">
-
-        <div class="well">
-            <h2>
-                <?php echo $category['name'] ?>
-            </h2>
-
-            <h3>
-                <?php echo '[' . $category['description'] . ']' ?>
-            </h3>
+<main>
+    <div class="col-sm-12 clearfix">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3>
+                    <?php echo $category['name'] ?>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <h4>
+                    <?php echo '[' . $category['description'] . ']' ?>
+                </h4>
+            </div>
         </div>
 
-        <?php foreach ($questions as $question) {
-            ?>
-
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <a href="<?= ABS_ROOT_URL ?>questions/details/<?= $question['id'] ?>">
-                            <h3><?= $question['title'] ?></h3>
-                        </a>
-                    </div>
-                    <div class="panel-body">
-                        <a href="#" class="btn btn-info">Votes</a>
-                        <a href="#" class="btn btn-success">Answers</a>
-                        <!--                        <p class="text-info">--><? //= $question['text'] ?><!--</p>-->
-
-
-                    </div>
+        <?php foreach ($questions as $question) : ?>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <a href="<?= ABS_ROOT_URL ?>questions/details/<?= $question['id'] ?>">
+                        <h4><strong>
+                                <span class="glyphicon glyphicon-user"> </span> *userID* =
+                                <?= $question['user_id'] ?>
+                            </strong>
+                            <em> asked:
+                                <?= $question['title'] ?>
+                            </em>
+                        </h4>
+                    </a>
                 </div>
 
-        <?php
-        }
-        ?>
+                <div class="panel-body">
+                    <p class="text-info"><?php
+                        echo substr($question['text'], 0, 100);
+                        if (strlen($question['text'])):
+                            echo "...";
+                        endif;
+                        ?>
+                    </p>
+                </div>
 
-</div>
-<?php
+                <div class="panel-footer">
+                <span class="text-muted">
+                    <span class="glyphicon glyphicon-time"></span>
+                    <span class="label label-default"><?= $question['time_created'] ?></span>
+                </span>
+                    &nbsp;
+                <span class="text-info">
+                    <span class="glyphicon glyphicon-eye-open"></span>
+                    <span class="label label-info"><?= $question['times_viewed'] ?>
+                        <?php
+                        if ($question['times_viewed'] == 1) {
+                            echo(" view");
+                        } else {
+                            echo(" views");
+                        }
+                        ?>
+                    </span>
+                    &nbsp;
+                </span>
+                    <!--        to-be removed? -->
+                    <!--        to-be removed? -->
+                <span class="text-warning">
+                <span class="glyphicon glyphicon-plus-sign"></span>
+                <span class="label label-warning"><?= $question['vote_result'] ?>
+                    <?php
+                    if ($question['vote_result'] == 1) {
+                        echo(" vote");
+                    } else {
+                        echo(" votes");
+                    }
+                    ?>
+                </span>
+                </span>
+                    <!--        to-be removed? -->
+                    <!--        to-be removed? -->
+                </div>
+
+            </div>
+        <?php endforeach; ?>
+    </div>
+</main>
--- views/questions/details.php
+++ views/questions/details.php
@@ -1,27 +1,47 @@
-<div class="panel panel-primary">
-    <div class="panel-heading">
-        <h3 class="panel-title">
-            <span class="glyphicon glyphicon-user"></span>
-            <span><strong> <?= $question['question'][0]['author'] ?></strong></span>
-            <span><em> asked: </em></span>
-            <?= $question['question'][0]['title'] ?>
-        </h3>
-    </div>
-    <div class="panel-body">
-        <?= $question['question'][0]['text'] ?>
-    </div>
-    <div class="panel-footer">
-        <span class="glyphicon glyphicon-time"></span>
-        <span class="label label-default"><?= $question['question'][0]['time_created'] ?></span>
-        <span class="glyphicon glyphicon-book"></span>
-        <span class="label label-primary"><?= $question['question'][0]['category'] ?></span>
-        <!--        to-be removed-->
-        <!--        to-be removed-->
-        <span class="label label-warning">votes: <?= $question['question'][0]['vote_result'] ?></span>
-        <!--        to-be removed-->
-        <!--        to-be removed-->
-        <p class="text-info">
-            <span class="glyphicon glyphicon-tag"></span>
+<main>
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">
+                <span class="glyphicon glyphicon-user"></span>
+                <span><strong> <?= $question['question'][0]['author'] ?></strong></span>
+            <span><em> asked:
+                    <?= $question['question'][0]['title'] ?>
+                </em></span>
+            </h3>
+        </div>
+        <div class="panel-body">
+            <?= $question['question'][0]['text'] ?>
+        </div>
+        <div class="panel-footer">
+        <span class="text-muted">
+            <span class="glyphicon glyphicon-time"></span>
+            <span class="label label-default"><?= $question['question'][0]['time_created'] ?></span>
+        </span>
+            &nbsp;
+        <span class="text-primary">
+            <span class="glyphicon glyphicon-book"></span>
+            <span class="label label-primary"><?= $question['question'][0]['category'] ?></span>
+        </span>
+            &nbsp;
+            <!--        to-be removed-->
+            <!--        to-be removed-->
+         <span class="text-warning">
+                <span class="glyphicon glyphicon-plus-sign"></span>
+        <span class="label label-warning"><?= $question['question'][0]['vote_result'] ?>
+            <?php
+            if ($question['question'][0]['vote_result'] == 1) {
+                echo(" vote");
+            } else {
+                echo(" votes");
+            }
+            ?>
+        </span>
+
+             </span>
+            <!--        to-be removed-->
+            <!--        to-be removed-->
+            <p class="text-info">
+                <span class="glyphicon glyphicon-tag"></span>
             <span>
                 <?php
                 foreach ($question['tags'] as $tag) : ?>
@@ -29,26 +49,29 @@
                 <?php
                 endforeach ?>
             </span>
-        </p>
-    </div>
-</div>
-<?php foreach ($question['answers'] as $answer) :
-    ?>
-    <div class="panel panel-success">
-        <div class="panel-heading">
-            <h3 class="panel-title">
-                <span class="glyphicon glyphicon-user"></span>
-                <span><strong><?= $answer['username'] ?></strong></span>
-                <span><em> answered:</em></span>
-            </h3>
+            </p>
         </div>
+    </div>
+    <?php foreach ($question['answers'] as $answer) :
+        ?>
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    <span class="glyphicon glyphicon-user"></span>
+                    <span><strong><?= $answer['username'] ?></strong></span>
+                    <span><em> answered:</em></span>
+                </h3>
+            </div>
 
-        <div class="panel-body">
-            <?= $answer['text'] ?>
+            <div class="panel-body">
+                <?= $answer['text'] ?>
+            </div>
+            <div class="panel-footer">
+            <span class="text-muted">
+                <span class="glyphicon glyphicon-time"></span>
+                <span class="label label-default"><?= $answer['time_created'] ?></span>
+            </span>
+            </div>
         </div>
-        <div class="panel-footer">
-            <span class="glyphicon glyphicon-time"></span>
-            <span class="label label-default"><?= $answer['time_created'] ?></span>
-        </div>
-    </div>
-<?php endforeach; ?>
+    <?php endforeach; ?>
+</main>
