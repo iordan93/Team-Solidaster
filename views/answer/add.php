f2579582077bbfc8e8d797dc509f6422808e1149
DinkoAndreev <dinkoandreev@gmail.com>
2014-08-26 18:10:25 +0300

    add answer and add comment style

--- views/answer/add.php
+++ views/answer/add.php
@@ -1,5 +1,18 @@
-<form method='post'>
-    <label>Answer:</label>
-    <input type='text' name='answerName' />
-    <input type='submit' value='Add answer' /> 
-</form>
+<div class="well bs-component">
+    <form method="post" class="form-horizontal">
+        <fieldset>
+            <legend>Add answer</legend>
+            <div class="form-group">
+                <label for="answerName" class="col-lg-2 control-label">Answer</label>
+                <div class="col-lg-10">
+                    <input type="text" class="form-control" ID="answerName" name='answerName' placeholder="Text" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-lg-10 col-lg-offset-2">
+                    <input type="submit" class="btn btn-primary" value="Add answer"/>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+</div>
