9e653ea7c42693421fee8b436fc54feb3110e110
Yordan Darakchiev <iordan93@gmail.com>
2014-08-26 21:15:42 +0300

    Question details bugfix - counter was not updating itself

--- models/QuestionModel.php
+++ models/QuestionModel.php
@@ -14,7 +14,9 @@ class QuestionModel extends BaseModel
 
     public function getConciseDisplay()
     {
-        $query = "SELECT q.id AS id, q.title AS title, q.time_created AS time_created, q.times_viewed, q.vote_result AS vote_result, u.username AS author, (SELECT COUNT(1) AS A1 FROM answers AS a WHERE q.id = a.questions_id)AS answers_count, c.name AS category FROM questions AS q INNER JOIN categories AS c ON q.category_id = c.id INNER JOIN users AS u on q.user_id = u.id";
+        $query = "SELECT q.id AS id, q.title AS title, q.time_created AS time_created, q.times_viewed,
+        q.vote_result AS vote_result, u.username AS author, (SELECT COUNT(1) AS A1 FROM answers AS a WHERE q.id = a.questions_id)AS answers_count,
+        c.name AS category, c.id as category_id FROM questions AS q INNER JOIN categories AS c ON q.category_id = c.id INNER JOIN users AS u on q.user_id = u.id";
         $resultSet = $this->dbConnection->query($query);
         return self::processResults($resultSet);
     }
@@ -29,7 +31,6 @@ join users as u
 on q.user_id = u.id
 where q.id = {$id}";
         $question = self::processResults($this->dbConnection->query($query));
-
         $query = "select tags.name from (select q.id, t.name
 from questions as q
 left outer join questions_tags as qt
