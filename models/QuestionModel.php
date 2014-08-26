021bf27c14c9cac0b8df395016a4080e9a313fa6
Yordan Darakchiev <iordan93@gmail.com>
2014-08-26 16:58:39 +0300

    Many fixes

--- models/QuestionModel.php
+++ models/QuestionModel.php
@@ -11,7 +11,7 @@ class QuestionModel extends BaseModel {
     }
 
     public function getConciseDisplay() {
-        $query = "SELECT q.id AS id, q.title AS title, q.time_created AS time_created, q.vote_result AS vote_result, u.username AS author, (SELECT COUNT(1) AS A1 FROM answers AS a WHERE q.id = a.questions_id)AS answers_count, c.name AS category FROM questions AS q INNER JOIN categories AS c ON q.category_id = c.id INNER JOIN users AS u on q.user_id = u.id";
+        $query = "SELECT q.id AS id, q.title AS title, q.time_created AS time_created, q.times_viewed, q.vote_result AS vote_result, u.username AS author, (SELECT COUNT(1) AS A1 FROM answers AS a WHERE q.id = a.questions_id)AS answers_count, c.name AS category FROM questions AS q INNER JOIN categories AS c ON q.category_id = c.id INNER JOIN users AS u on q.user_id = u.id";
         $resultSet = $this->dbConnection->query($query);
         return self::processResults($resultSet);
     }
