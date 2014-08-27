95a5c779f58d7fdec81900ca52d1071b52e09db8
DinkoAndreev <dinkoandreev@gmail.com>
2014-08-27 03:49:46 +0300

    profile/show view

--- views/profile/show.php
+++ views/profile/show.php
@@ -10,44 +10,27 @@ var_dump($comments);
         <li class="active"><a href="#home" data-toggle="tab">Questions</a></li>
         <li><a href="#profile" data-toggle="tab">Answers</a></li>
         <li class="disabled"><a>Disabled</a></li>
-        <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                Profile <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="#dropdown1" data-toggle="tab">Info</a></li>
-                <li class="divider"></li>
-                <li><a href="#dropdown2" data-toggle="tab">Settings</a></li>
-            </ul>
-        </li>
     </ul>
     <div id="myTabContent" class="tab-content">
         <div class="tab-pane fade active in" id="home">
-            <table class="table table-striped table-hover ">
-                <thead>
-                <tr>
-                    <th>Question title</th>
-                    <th>Question description</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($questions as $question) {
-                        echo "<tr><td><a href=\"" . ABS_ROOT_URL . "questions/details/{$question['id']}\">{$question["title"]}</a></td><td>{$question["text"]}</td></tr>";
-                    } ?>
-                </tbody>
-            </table>
+            <?php foreach($questions as $question) {
+                echo "<div>
+                        <a href=\"" . ABS_ROOT_URL . "questions/details/{$question['id']}\">{$question["title"]}</a>
+                      </div>";
+                echo substr("<div>{$question["text"]}</div>", 0, 100);
+                if (strlen($question['text']) > 100):
+                    echo "...";
+                endif;
+            } ?>
         </div>
         <div class="tab-pane fade" id="profile">
             <?php foreach($answers as $answer) {
-            echo "<div><a href=\"" . ABS_ROOT_URL . "questions/details/{$answer['questions_id']}\">{$answer["title"]}</a></div>";
-            echo "<div>{$answer["text"]}</div>";
+                echo "<div><a href=\"" . ABS_ROOT_URL . "questions/details/{$answer['questions_id']}\">{$answer["title"]}</a></div>";
+                echo substr("<div>{$answer["text"]}</div>", 0, 100);
+                if (strlen($answer['text']) > 100):
+                    echo "...";
+                endif;
             } ?>
-        </div>
-        <div class="tab-pane fade" id="dropdown1">
-            <p></p>
-        </div>
-        <div class="tab-pane fade" id="dropdown2">
-            <p></p>
         </div>
     </div>
 </div>
