3a010fce01f2a4697c817d7b3e8d9cbdd55b649d
Krasimir <k_garbuchev@yahoo.com>
2014-08-27 13:56:12 +0300

    Add Comment Update

--- views/answers/add.php
+++ views/answers/add.php
@@ -1,15 +1,17 @@
-<form method="post">
-    <fieldset>
-        <legend>Add Answer:</legend>
-        <textarea rows="15" name="commentText" class="col-sm-11"></textarea>
-    </fieldset>
-    &nbsp;
+<div class="jumbotron">
+    <form method="post">
+        <fieldset>
+            <legend>Add Answer:</legend>
+            <textarea rows="15" name="commentText" class="col-sm-11"></textarea>
+        </fieldset>
+        &nbsp;
 
-    <fieldset>
-        <div class="form-group col-sm-12 row-lg-offset-2">
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <a class="btn btn-default"
-               href="<?= ABS_ROOT_URL ?>questions/details/<?= $question["id"] ?>">Cancel</a>
-        </div>
-    </fieldset>
-</form>
+        <fieldset>
+            <div class="form-group col-sm-12 row-lg-offset-2">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <a class="btn btn-default"
+                   href="<?= ABS_ROOT_URL ?>questions/details/<?= $question["id"] ?>">Cancel</a>
+            </div>
+        </fieldset>
+    </form>
+</div>
--- views/comments/add.php
+++ views/comments/add.php
@@ -1,14 +1,16 @@
-<form method="post" class="form-horizontal">
-    <fieldset>
-        <legend>Add Comment:</legend>
-        <textarea rows="15" name="commentText" class="col-sm-11"></textarea>
-    </fieldset>
-    &nbsp;
-    <fieldset>
-        <div class="form-group col-sm-12 row-lg-offset-2">
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <a class="btn btn-default"
-               href="<?= ABS_ROOT_URL ?>questions/details/<?= $answer["questions_id"] ?>">Cancel</a>
-        </div>
-    </fieldset>
-</form>
+<div class="jumbotron">
+    <form method="post" class="form-horizontal">
+        <fieldset>
+            <legend>Add Comment:</legend>
+            <textarea rows="15" name="commentText" class="col-sm-11"></textarea>
+        </fieldset>
+        &nbsp;
+        <fieldset>
+            <div class="form-group col-sm-12 row-lg-offset-2">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <a class="btn btn-default"
+                   href="<?= ABS_ROOT_URL ?>questions/details/<?= $answer["questions_id"] ?>">Cancel</a>
+            </div>
+        </fieldset>
+    </form>
+</div>
