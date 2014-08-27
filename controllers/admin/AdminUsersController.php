09b56bca355c92152f825036250dd46ec1912a80
Yordan Darakchiev <iordan93@gmail.com>
2014-08-27 11:49:18 +0300

    Admin categories adding and editing implemented

--- controllers/admin/AdminUsersController.php
+++ controllers/admin/AdminUsersController.php
@@ -1,6 +1,19 @@
 <?php
 namespace Admin\Controllers;
 
-class AdminUsersController {
+class AdminUsersController extends BaseAdminController {
+    public function __construct($viewsDirectory = "", $layout = "", $model = "user", $auxModels = array())
+    {
+        parent::__construct($viewsDirectory, $layout, $model, $auxModels);
+    }
 
+
+    public function index()
+    {
+        header("Location: " . ABS_ADMIN_ROOT_URL);
+    }
+
+    public function edit() {
+
+    }
 } 
